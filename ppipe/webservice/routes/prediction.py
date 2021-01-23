2140003de4fbbb238760f913176b3c68df7b2584
Arnau Diaz <diazarnau0@gmail.com>
2021-01-23 18:52:11 +0100

    refactor: pydantic support

--- ppipe/webservice/routes/prediction.py
+++ ppipe/webservice/routes/prediction.py
@@ -1,14 +1,15 @@
-from dataclasses import dataclass
+from pydantic import BaseModel
+
 from fastapi import APIRouter, Depends
 from starlette.requests import Request
 
 from ..core import security
+from ..core.messages import NO_VALID_PAYLOAD
 
 router = APIRouter()
 
 
-@dataclass
-class PredictionRequest:
+class PredictionRequest(BaseModel):
     value: int
 
 
@@ -18,4 +19,6 @@ def post_predict(
     authenticated: bool = Depends(security.validate_request),
     prediction_request: PredictionRequest = None
 ) -> int:
+    if prediction_request is None:
+        raise ValueError(NO_VALID_PAYLOAD.format(prediction_request))
     return prediction_request.value + 1
