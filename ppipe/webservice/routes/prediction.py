c72309509f9630a692e20b66b982e07687366910
Arnau Diaz <diazarnau0@gmail.com>
2021-01-23 18:43:05 +0100

    feature: prediction request model

--- ppipe/webservice/routes/prediction.py
+++ ppipe/webservice/routes/prediction.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from fastapi import APIRouter, Depends
 from starlette.requests import Request
 
@@ -6,9 +7,15 @@ from ..core import security
 router = APIRouter()
 
 
+@dataclass
+class PredictionRequest:
+    value: int
+
+
 @router.post("/predict", name="predict")
 def post_predict(
     request: Request,
     authenticated: bool = Depends(security.validate_request),
-) -> bool:
-    return True
+    prediction_request: PredictionRequest = None
+) -> int:
+    return prediction_request.value + 1
